3a804c6c9b04ba67ca3e14ef50f83cea3819712b
Hasan Aydoğdu <hsnaydd@gmail.com>
2017-10-10 22:40:07 +0300

    Completed Card component and applied in Statistic page

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ npm test
 ```
 
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+# Credits
+
+## Icons
+Car Cost by [Symbolon](https://thenounproject.com/symbolon/) from the Noun Project
+Time by [Alfa Design](https://thenounproject.com/alfadesign) from the Noun Project
---
+++ src/components/ui/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card bg-light mb-3">
+    <div class="card-header">{{title}}</div>
+    <div class="card-body">
+      <div class="card-stats">
+        <slot name="icon"></slot>
+        <span class="card-count">{{count}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-stats {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-count {
+  color: var(--color-oslo-gray);
+  font-size: 32px;
+  margin-left: 18px;
+}
+</style>
+
+<script>
+export default {
+  name: 'Card',
+  props: [
+    'title',
+    'count'
+  ]
+};
+</script>
